5783b841efe4d20d2d610b626dddc4e609dedb08
Christopher <visrule@protonmail.com>
2021-05-16 18:29:40 -0500

    Initial Commit

---
+++ AvoLife/AvoLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvoLifeApp.swift
+//  AvoLife
+//
+//  Created by Christopher Hicks on 5/16/21.
+//
+
+import SwiftUI
+
+@main
+struct AvoLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AvoLife/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AvoLife
+//
+//  Created by Christopher Hicks on 5/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
